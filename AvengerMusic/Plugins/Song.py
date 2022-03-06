984f91c73d2d54864dfdcfee91d8405f14a7d6c7
chaprimayank <94171814+chaprimayank@users.noreply.github.com>
2022-03-06 15:37:54 +0530

    Update Song.py

--- AvengerMusic/Plugins/Song.py
+++ AvengerMusic/Plugins/Song.py
@@ -23,7 +23,7 @@ __HELP__ = """
 
 
 @app.on_message(
-    filters.command(["song", f"song@{BOT_USERNAME}"]) & filters.group
+    filters.command(["video", f"video@{BOT_USERNAME}"]) & filters.group
 )
 @PermissionCheck
 async def play(_, message: Message):
